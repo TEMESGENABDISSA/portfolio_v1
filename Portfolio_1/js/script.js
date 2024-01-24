24295e01887dc85313500f587d1f59310e1688af
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2024-01-25 00:32:50 +0300

    Temesgen Abdissa Hands on simple Portfolio

---
+++ Portfolio_1/js/script.js
@@ -0,0 +1,11 @@
+
+        /*==================== typed js ====================*/
+        
+        const typed = new Typed('.multiple-text', {
+            Strings: ['Full Stack Software Engineer!', 'Full Stack Web Developer!'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true
+            });
+
